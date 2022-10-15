25a27f1d50d62abebc96f80fab457ba6aa37ddbb
clubsolar <hira@clubsolar.com>
2022-10-15 09:13:10 -0400

    moved button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'aurora-ai';
+  // mobile:boolean = false;
+
+  mobile = true;
+
+  constructor() {}
+
+
+  ngOnInit(): void {
+    if (window.innerWidth < 500) {
+      this.mobile = true;
+      console.log(this.mobile);
+    }
+    else {
+      this.mobile = false;
+      console.log(this.mobile);
+    }
+  }
+
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ɵflushModuleScopingQueueAsMuchAsPossible } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,16 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+const mobile = true;
+
+if (window.innerWidth < 500) {
+  const mobile = true;
+  console.log(mobile);
+}
+else {
+  const mobile = false;
+  console.log(mobile);
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule, {providers: [{provide: mobile, useValue: mobile}]})
   .catch(err => console.error(err));
